8779567a69a3e5150e6887008c8582be2950af2a
mohammadalidev2026-star <mohammadali.dev2026@gmail.com>
2026-07-20 18:38:01 +0430

    feat(setup project)

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import reactLogo from "./assets/react.svg";
+import viteLogo from "./assets/vite.svg";
+import heroImg from "./assets/hero.png";
+
+function App() {
+  return (
+    <>
+      <div>hello world</div>
+    </>
+  );
+}
+
+export default App;
